daf2dca0eca06141bee51006e79f950fc4b29261
mouadHILLALI <mauadtoto001@gmail.com>
2025-08-17 21:21:44 +0100

    creating a new branch for device discovery

--- src/main/java/com.mouad.twotooth/infrastructure/adpater/bleutooth/BluetoothDevicesScanner.java
+++ src/main/java/com.mouad.twotooth/infrastructure/adpater/bleutooth/BluetoothDevicesScanner.java
@@ -24,26 +24,32 @@ public class BluetoothDevicesScanner implements DeviceScannerPort, DiscoveryList
         } catch (Exception e) {
             e.printStackTrace();
         }
-    return foundDevices;
+        return foundDevices;
     }
 
     @Override
     public void deviceDiscovered(RemoteDevice remoteDevice, DeviceClass deviceClass) {
+        try {
+            String address = remoteDevice.getBluetoothAddress();
+            String name = remoteDevice.getFriendlyName(false);
+            foundDevices.add(new BluetoothDevice(address, name));
+        } catch (Exception e) {
+            foundDevices.add(new BluetoothDevice(remoteDevice.getBluetoothAddress(), "unknown"));
+        }
 
     }
 
     @Override
-    public void servicesDiscovered(int i, ServiceRecord[] serviceRecords) {
-
-    }
+    public void servicesDiscovered(int i, ServiceRecord[] serviceRecords) {}
 
     @Override
-    public void serviceSearchCompleted(int i, int i1) {
-
-    }
+    public void serviceSearchCompleted(int i, int i1) {}
 
     @Override
     public void inquiryCompleted(int i) {
-
+        synchronized (inquiryCompletedEvent) {
+            inquiryCompletedEvent.notifyAll();
+        }
     }
 }
+
